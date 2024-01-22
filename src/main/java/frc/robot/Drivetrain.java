0c08f6d230ae454ed7aed5265a6d61edd4120795
AnthonyOfTulsa <AnthonyOfTulsa@gmail.com>
2024-01-22 12:22:59 -0600

    Adding initial SwerveModule and Drivetrain

---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,7 @@
+package frc.robot;
+
+// The interface to control our drivetrain (swerve drive).
+public class Drivetrain {
+    
+    
+}
---
+++ src/main/java/frc/robot/SwerveModule.java
@@ -0,0 +1,7 @@
+package frc.robot;
+
+// Contains the interface to a single Swerve Module.
+// There will be 4 instances of this class, one for each swerve module on our robot.
+// However, we only have to write the code once, and will reuse it by creating a SwerveModule variable for each module.
+public class SwerveModule {    
+}
