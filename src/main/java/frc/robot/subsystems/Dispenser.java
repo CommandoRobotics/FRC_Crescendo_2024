248f9f9e7fcf9dc468e2d9a1971762ff16e5bd96
CasciaRobots <CasciaRobots@gmail.com>
2024-03-30 10:27:48 -0500

    cleaned up code

--- src/main/java/frc/robot/subsystems/Dispenser.java
+++ src/main/java/frc/robot/subsystems/Dispenser.java
@@ -5,25 +5,14 @@
 // The FRC package is something the RoboRio code looks for so it can run our code.
 package frc.robot.subsystems;
 
-// The imports include classes from various code libraries.
-// They contain prewritten code we can use to make our job easier.
-// The order does not affect the program, but we usually place the libraries from WPI first,
-// and the place the others in alphabetical order, just so it is easy to read and conistent.
-import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.math.MathUtil;
-
-import java.util.function.DoubleSupplier;
-
 import java.util.function.DoubleSupplier;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -33,40 +22,21 @@ import frc.robot.Constants.DispenserConstants;
 // This class controls the internal electronics of the Dispenser as well as providing
 // an interface for controlling it.
 public class Dispenser extends SubsystemBase {
-    // We usually start with variables that will be accessible/shared by all code in our class.
-    // These variables are called "members" of our class. The WPI Lib settings encourage us to
-    // begin member variable names with "m_" (i.e. m_someNumber).
-    // Since other code might not know how to properly manipulate the variables in this class,
-    // we usually mark the variables as "private". This prevents other classes from messing with
-    // them.
-    // Variables markes as "final" mean that its value cannot be changed after it is set. We can
-    // use "final" to ensure we don't accidently overwrite values. If you try to change the value
-    // of a variable marked as "final", the compiler will generate an error. Values for variables
-    // marked as final can be set here. Other variables have to be set in the constructor.
-    // Using "private" and "final" are not required, but using them in the appropriate situations
-    // can help avoid improper access and prevent bugs in our code.
 
     // Declare all the motors that will be used in this class.
     private CANSparkMax m_intakeMotor;
     private CANSparkMax uppershooterMotor;
     private CANSparkMax lowershooterMotor;
-     // Motor controlling the upper roller on the shooter.
+
+    //Declares the sensors used in this class
     private DigitalInput m_intakeBeamBreak;
     private DigitalInput m_indexerBeamBreak;
     private DigitalInput m_shooterBeamBreak;
     
-    // This special function is known as the constructor for the class. Notice that the constructor
-    // has the EXACT SAME name (including capitalization) as our class' name. Also note that the
-    // constructor does not have a return type (this is unlike other functions).
-    // The constructor is where we set initial values and run any initial setup code.
-    // Since our Robot class (in Robot.java) has a Dispenser as a member variable (m_dispenser),
-    // this function will be called whenever our code starts: either on power-on, or when new
-    // code is downloaded, or when the robot code is restarted following a code crash.
-    public Dispenser() {
-        // Set the information for our member variables.
 
-        // Set the motors to the appropriate CAN device ID and set the type (brushed or brushless).
-    
+    public Dispenser() {
+
+    // Set the motors to the appropriate CAN device ID and set the type (brushed or brushless).   
         m_intakeMotor = new CANSparkMax(41, MotorType.kBrushless);
         uppershooterMotor = new CANSparkMax(53, MotorType.kBrushless);
         lowershooterMotor = new CANSparkMax(51, MotorType.kBrushless);
@@ -80,10 +50,8 @@ public class Dispenser extends SubsystemBase {
         m_intakeMotor.setIdleMode(IdleMode.kCoast);
         uppershooterMotor.setIdleMode(IdleMode.kCoast);
         lowershooterMotor.setIdleMode(IdleMode.kCoast);
-        // Set the motor as "inverted", meaining when we tell it to operate at a speed, it will
-        // actually turn in reverse at that speed (i.e. -50% when we tell it +50%). This is because
-        // we think of positive speeds as brining in a Note, but the motor is rotate such that positive
-        // would normally spit the Note out.
+
+        //set motor inversions and amp limits
         m_intakeMotor.setInverted(false);
         uppershooterMotor.setInverted(false);
         m_intakeMotor.setSmartCurrentLimit(40);
@@ -101,32 +69,37 @@ public class Dispenser extends SubsystemBase {
         return run(() -> release());
     }
 
+    //allows for manual control of the shooter
     public Command manualSpinCommand(DoubleSupplier threshold) {
         return run( () -> spinThreshold(threshold.getAsDouble()));
     }
 
+    //revs up the shooter 
     public Command spinCommand() {
         return run(() -> spinUpShooterWheels());
     }
 
- 
-
+    //intakes w/ beambreaks
     public Command autoIntakeCommand() {
         return run(() -> autoIntake());
     }
 
+    //shoots whatevers in the robot
     public Command forceShootCommand() {
         return run(() -> feedShooter());
     }
 
+    //shoots until empty
     public Command shootUntilEmptyCommand() {
         return run(() -> feedShooter());
     }
 
+    //ejects whatevers in the robot out the intake
     public Command ejectNoteCommand() {
         return run(() -> ejectNote());
     }
 
+    //sets speed to put note in the amp
     public Command ampDispenseCommand() {
         return run(() -> setDispenser(DispenserConstants.kAmpDispenseSpeed));
     }
@@ -136,6 +109,7 @@ public class Dispenser extends SubsystemBase {
         return run(() -> setDispenser(speed.getAsDouble()));
     }
 
+    //sets speed of the intake at the source
     public Command sourceIntakeCommand() {
         return run(() -> intakeAtSpeed(DispenserConstants.kSourceIntakeSpeed));
     }
@@ -144,8 +118,6 @@ public class Dispenser extends SubsystemBase {
     public Command intakeAtSpeedCommand(DoubleSupplier speed) {
         return run(() -> intakeAtSpeed(speed.getAsDouble()));
     }
-
-
 
 
     public Command manualShootCommand(DoubleSupplier speed) {
@@ -284,9 +256,7 @@ public class Dispenser extends SubsystemBase {
         }
     }
 
-    // This function runs the shooter motors, while keeping the intake system stopped.
-    // Since the shooter motors take some amount of time to get to their max speed, we would run
-    // this function before we actually call shootNoteImmediately().
+    //revs up shooter
     public void spinUpShooterWheels() {
         // Stop the intake (and force brake mode so a Note can't roll out).
         m_intakeMotor.set(0);
@@ -297,6 +267,7 @@ public class Dispenser extends SubsystemBase {
     }
 
 
+    //revs up the shooter slowly
     public void spinUpShooterWheelsSlow() {
         // Stop the intake (and force brake mode so a Note can't roll out).
         m_intakeMotor.set(0);
@@ -331,8 +302,6 @@ public class Dispenser extends SubsystemBase {
     }
 
     // The following sends information about this subsystem to the Smart Dashboard.
-
-
     @Override
     public void periodic() {
         // The arm is perpendicular to the Upright shoulder.
